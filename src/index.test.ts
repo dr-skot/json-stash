8508be8849ae1243ac216d7dd995c4fb5ae5a49f
Scott Shepherd <skot@pobox.com>
2023-03-19 08:00:18 -0400

    feat: add toJson and fromJson, supporting duplicate & circular references

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,48 @@
-import { hello } from "./index";
+import { fromJSON, toJSON } from "./index";
+describe("toJSON", () => {
+  it("handles ordinary objects", () => {
+    const inputs = [
+      null,
+      { a: 1, b: { c: 3 }, d: [4, 5, 6], e: undefined },
+      [1, 2, 3],
+      1,
+      "a",
+      true,
+      false,
+    ];
+    inputs.forEach((input) => {
+      expect(toJSON(input)).toEqual(JSON.stringify(input));
+      expect(fromJSON(toJSON(input))).toEqual(input);
+    });
+  });
 
-describe("hello", () => {
-  it("should return hello", () => {
-    expect(hello()).toEqual("hello");
+  it("restores duplicate properties", () => {
+    const a = [1, 2, 3];
+    const obj = { orig: a, same: a, copied: [...a] };
+    expect(obj.same).toBe(obj.orig);
+    expect(obj.copied).not.toBe(obj.orig);
+    const deserialized = fromJSON(toJSON(obj));
+    expect(deserialized.same).toBe(deserialized.orig);
+    expect(deserialized.copied).not.toBe(deserialized.orig);
+  });
+
+  it("restores circular refs", () => {
+    const obj: { self?: unknown; num: number } = { num: 2 };
+    obj.self = obj;
+    const serialized = toJSON(obj);
+    expect(serialized).toEqual('{"num":2,"self":{"_refId":"$"}}');
+    const deserialized = fromJSON(serialized);
+    expect(deserialized).toEqual(obj);
+    expect(deserialized.self).toBe(deserialized);
+
+    // a more nested example
+    const obj2 = { a: 1, b: [4, 5, obj], c: undefined };
+    const serialized2 = toJSON(obj2);
+    expect(serialized2).toEqual(
+      '{"a":1,"b":[4,5,{"num":2,"self":{"_refId":"$.b.2"}}]}'
+    );
+    const deserialized2 = fromJSON(serialized2);
+    expect(deserialized2).toEqual(obj2);
+    expect(deserialized2.b[2]).toBe(deserialized2.b[2].self);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,109 @@
-export function hello() {
-  console.log('hello');
-  return "hello";
+import _ from "lodash";
+
+//
+// exports
+//
+
+export function toJSON(raw: unknown) {
+  return JSON.stringify(circularToRefs(raw));
+}
+
+export function fromJSON(json: string) {
+  const result = JSON.parse(json);
+  refsToCircular(result);
+  return result;
+}
+
+//
+// internals
+//
+
+// a ref is a placeholder for a value that has already been seen
+// the _refId is a path to the value in some reference object
+type Ref = {
+  _refId: string;
+};
+
+// a function that tracks values and returns a ref when a value is repeated
+// (if the value is an object or an array)
+// otherwise returns the value
+type RefSaver = (path: string, value: unknown) => unknown;
+
+function getRefSaver(): RefSaver {
+  const seen: Record<string, object> = {};
+  return (path, value) => {
+    if (value === null || typeof value !== "object") return value;
+    const found = Object.entries(seen).find(([, v]) => v === value);
+    if (found) return { _refId: found[0] };
+    seen[path] = value;
+    return value;
+  };
+}
+
+function circularToRefs(raw: unknown) {
+  return valueToRefs(raw, "$", getRefSaver());
+}
+
+function valueToRefs(
+  rawValue: unknown,
+  path: string,
+  refSaver: RefSaver
+): unknown {
+  const value = refSaver(path, rawValue);
+  // if value is not raw, we have a ref, so return it
+  if (value !== rawValue) return value;
+  if (_.isArray(value))
+    return (value as unknown[]).map((v, i) =>
+      valueToRefs(v, `${path}.${i}`, refSaver)
+    );
+  if (_.isObject(value))
+    return Object.fromEntries(
+      Object.entries(value as object).map(([k, v]) => [
+        k,
+        valueToRefs(v, `${path}.${k}`, refSaver),
+      ])
+    );
+  return value;
+}
+
+function refsToCircular(value: unknown) {
+  if (!_.isObject(value)) return;
+  valueToCircular(value, "$", value as object);
+}
+
+function valueToCircular(
+  rawValue: unknown,
+  path: string,
+  context: object
+): unknown {
+  if (rawValue === null || typeof rawValue !== "object") return;
+  const { _refId } = rawValue as Ref;
+  if (_refId) {
+    const referent = getPath(context, _refId);
+    setPath(context, path, referent);
+    return;
+  }
+  if (_.isArray(rawValue)) {
+    (rawValue as unknown[]).forEach((v, i) =>
+      valueToCircular(v, `${path}.${i}`, context)
+    );
+    return;
+  }
+  if (_.isObject(rawValue)) {
+    Object.entries(rawValue).forEach(([k, v]) =>
+      valueToCircular(v, `${path}.${k}`, context)
+    );
+    return;
+  }
+}
+
+function getPath(context: object, rawPath: string) {
+  const path = rawPath.replace(/^\$\.?/, "");
+  return path ? _.get(context, path) : context;
+}
+
+function setPath(context: object, rawPath: string, value: unknown) {
+  const path = rawPath.replace(/^\$\.?/, "");
+  if (!path) throw new Error("Cannot set root path");
+  return _.set(context, path, value);
 }
