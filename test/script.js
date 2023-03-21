1d12c48a83376dedc3e161f3b25691e2eaa4aed9
Scott Shepherd <skot@pobox.com>
2023-03-21 14:20:18 -0400

    config: clean up dependencies

--- test/script.js
+++
@@ -1,8 +0,0 @@
-const { stash, unstash } = require("json-stash");
-
-// TODO elaborate these tests
-const d = { a: 1, b: 2, c: 3 };
-d.d = d;
-const e = { f: 4, g: [5, 6, 7], h: { i: 8, j: 9 } };
-console.log(stash(d));
-console.log(unstash(stash(d)));
