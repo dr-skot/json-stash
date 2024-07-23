9c5565fb61b13e2adf488977f454c506c59df1be
Scott Shepherd <skot@pobox.com>
2024-07-23 06:48:30 -0400

    types: insure exported @stashable decorator is correctly typed with .group() property

--- src/stashable.ts
+++ src/stashable.ts
@@ -22,7 +22,7 @@ function getKeyOrOpts(opts: StashableOptions) {
   };
 }
 
-export function stashable(opts: StashableOptions = {}) {
+function rawStashable(opts: StashableOptions = {}) {
   return function (target: any) {
     if (opts.group) {
       const group = groups[opts.group] || [];
@@ -34,4 +34,14 @@ export function stashable(opts: StashableOptions = {}) {
   };
 }
 
-stashable.group = (groupName: string) => groups[groupName] || [];
+rawStashable.group = (groupName: string) => groups[groupName] || [];
+
+interface StashableDecorator {
+  (opts?: StashableOptions): ClassDecorator;
+  group: (groupName: string) => StashableClassSpec[];
+}
+
+// TODO is there a more elegant way to jump through this typescript hoop?
+//   We want ts to know stashable is a function with a .group property
+//   I don't know how to accomplish that without this rename-and-cast hack
+export const stashable = rawStashable as StashableDecorator;
