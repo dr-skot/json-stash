c2553196e78f447bb6f3157525935716e7d796d9
Scott Shepherd <skot@pobox.com>
2024-07-25 10:09:38 -0400

    fix: make stashable.group() return suitable args for addClasses()

--- src/stashable.ts
+++ src/stashable.ts
@@ -1,28 +1,29 @@
 import { classSerializer, ClassSerializerOpts } from "./classSerializer";
-import { Serializer } from "./types/Serializer";
 import { addSerializers } from "./index";
+import { Class } from "./types/Class";
 
 // @stashable takes all the `addClass` options, plus a possible `group`
 interface StashableOpts extends ClassSerializerOpts<any> {
   group?: string;
 }
 
+type ClassSerializerSpec<T = any> = [Class<T>, ClassSerializerOpts<T>];
+
 // @stashable is a class decorator factory, with a `group()` method tacked onto it
 interface StashableDecorator {
   (opts?: StashableOpts): ClassDecorator;
-  group: (groupName: string) => Serializer[];
+  group: (groupName: string) => ClassSerializerSpec[];
 }
 
 // store groups of serializers
-const groups: Record<string, Serializer[]> = {};
+const groups: Record<string, ClassSerializerSpec[]> = {};
 
 // "raw" because we'll have to rename it to cast as StashableDecorator before exporting
 function rawStashable({ group, ...opts }: StashableOpts = {}) {
   return function (target: any) {
-    const serializer = classSerializer(target, opts);
     // add the serializer to the group, or to the global stasher if no group is specified
-    if (group) groups[group] = [...(groups[group] || []), serializer];
-    else addSerializers(serializer);
+    if (group) groups[group] = [...(groups[group] || []), [target, opts]];
+    else addSerializers(classSerializer(target, opts));
   };
 }
 
